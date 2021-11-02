42856cf74421775d3a7c5e94ed1356b5cc5bdbdd
6MyShadow9 <predator16101610@gmail.com>
2021-11-02 12:05:55 +0100

    Save state fixed, added floating text
    floating text doesnt 100% work

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
     public static GameManager instance;
     private void Awake()
     {
@@ -14,14 +15,12 @@ public class GameManager : MonoBehaviour
             return;
         }
 
-
         instance = this;
         SceneManager.sceneLoaded += LoadState;
         DontDestroyOnLoad(gameObject);
-        
     }
 
-    // Ressources
+    //resources
     public List<Sprite> playerSprites;
     public List<Sprite> weaponSprites;
     public List<int> weaponPrices;
@@ -29,20 +28,30 @@ public class GameManager : MonoBehaviour
 
     //references
     public Player player;
-    // public weapon weapon...
+    //public weapon weapon
+    public FloatingTextManager floatingTextManager;
 
-    //logic
+    //Logic
+
     public int pesos;
     public int experience;
 
-    /*
-    int preferedSkin
-    int pesos
-    int experience
-    int weaponLevel
-    */
+
+    //Flaoting text
+    public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
+    {
+        floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
+
+    }
+
 
     //Save state
+    /*
+    *int preferedSkin
+    *int pesos 
+    *int experience
+    *int weaponLevel
+    */
     public void SaveState()
     {
         string s = "";
@@ -50,18 +59,25 @@ public class GameManager : MonoBehaviour
         s += "0" + "|";
         s += pesos.ToString() + "|";
         s += experience.ToString() + "|";
-        s += "0";
+        s += "";
 
         PlayerPrefs.SetString("SaveState", s);
     }
 
     public void LoadState(Scene s, LoadSceneMode mode)
     {
-        string[] date = PlayerPrefs.GetString("SaveState").Split('|');
+
+        if(!PlayerPrefs.HasKey("SaveState"))
+            return;
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+
         //change player skin
         pesos = int.Parse(data[1]);
         experience = int.Parse(data[2]);
+        //Change the weapon level
 
-        Debug.Log("Load State");
+        Debug.Log("Load state");
     }
+
 }
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -10,7 +10,6 @@ public class Portal : Collidable
 
     protected override void OnCollide(Collider2D coll)
     {
-
         if(coll.name == "Player")
         {
             // Teleport player
